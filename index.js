069d7110a0d725dd4359200782e0d42ceb48781e
radiodario <dario.villanueva@gmail.com>
2016-05-26 09:33:52 +0100

    leaves everywhere

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var Leaf = require('./src/leaf');
 var d3 = require('d3');
+global.d3 = d3;
+require('d3-grid');
 var xmldom = require('xmldom');
 var jsdom = require("jsdom");
 var fs = require('fs');
 var document = jsdom.jsdom()
+var child_proc = require('child_process');
 
 
 //Lets require/import the HTTP module
@@ -14,28 +17,83 @@ const PORT=8080;
 
 //We need a function which handles requests and send response
 function handleRequest(request, response){
-  var height = 1500;
-  var width = 1500;
+  var width = 1440 * 4;
+  var height = 2560 * 4;
 
   var svg = d3.select(document.body).append("svg")
       .attr('xmlns', 'http://www.w3.org/2000/svg')
       .attr("width", width)
-      .attr("height", height)
+      .attr("height", height);
 
-  var l = Leaf(200, 250, false, new Date().getTime());
+  var line = d3.svg.line();
+
+  var grid = d3.layout.grid()
+    .bands()
+    .size([width, height])
+    .padding([0.0, 0.1]);
+
+
+  var ls = Array.apply(null, Array(1024)).map((d) => {
+    return {};
+  });
+
+
+  var r = svg.append('rect')
+    .attr('height', height)
+    .attr('width', width)
+    .style('fill', '#B1E9E8')
+
   var g = svg.append('g')
-    .attr('transform', 'translate(50, 50)')
-  g.append('path')
-    .attr('d', l.pointspath)
-    .style('fill', 'none')
-    .style('stroke', 'pink')
+    .attr('transform', 'translate(0, 0)')
 
-  g.append('path')
-    .attr('d', l.svgpath)
-    .style('fill', 'rgba(80, 200, 80, 0.8)')
-    .style('stroke', 'black')
+  var leaves = g.selectAll('g.leaf')
+      .data(grid(ls));
 
-    fs.writeFile('leaf.svg', svg.node().outerHTML);
+  var leafEnter = leaves.enter()
+    .append('g')
+    .attr('class', 'leaf')
+    .attr('transform', (d) => {
+      return `translate(${[d.x, d.y]})rotate(${Math.random() * 360})`;
+    })
+    .each((d) => {
+      d.l = Leaf(grid.nodeSize()[0], grid.nodeSize()[1],
+                 true, 100, new Date().getTime() + Math.random());
+    })
+
+  leafEnter.append('path')
+  .attr('d', (d) => {
+    return (line(d.l.side_l) + 'Z');
+  } )
+  .style('fill', (d) => d.l.color)
+  .style('fill-opacity', 0.8)
+  //'rgba(80, 200, 80, 0.8)')
+  .style('stroke', (d) => d.l.color)
+
+  leafEnter.append('path')
+  .attr('d', (d) => {
+    return (line(d.l.side_r) + 'Z');
+  } )
+  .style('fill', (d) => d.l.color)
+  .style('stroke', (d) => d.l.color )
+  .style('fill-opacity', 0.8)
+
+  fs.writeFile('leaf.svg', svg.node().outerHTML);
+  var a = require('url').parse(request.url)
+  if (a.pathname === '/png') {
+    var convert = child_proc.spawn("convert", ["svg:", "png:-"])
+
+    convert.stdout.on('data', function (data) {
+      response.write(data);
+    });
+    convert.on('exit', function(code) {
+      response.end();
+    });
+
+    convert.stdin.write(svg.node().outerHTML);
+    convert.stdin.end();
+    return;
+  }
+
   response.writeHead(200, {'Content-Type':"image/svg+xml"});
   response.end(svg.node().outerHTML);
 }
--- src/leaf.js
+++ src/leaf.js
@@ -1,27 +1,50 @@
 var procedural = require('procedural');
+var bspline = require('b-spline');
 
 var unilobe_without_basal = procedural('unilobe_without_basal')
   .takes('width')
   .takes('height')
   .takes('symmetric')
+  .takes('quality')
   .takes('randomicity')
+  .provides('color', (leaf) => {
+    const cols = [
+      '#e0f0d5',
+      '#c5e3af',
+      '#9ac37b',
+      '#72a24e',
+      '#54862e'
+    ]
+
+    const cherry = [
+      //'#501A4E',
+      '#C481B6',
+      '#FCC3E6',
+      '#FCC3E6',
+      '#FAF9F5'
+    ]
+    const rnd = leaf.getRandGen('color');
+    //return cherry[rnd.nextInt(0, cherry.length)];
+    return cols[rnd.nextInt(0, cols.length)];
+
+  })
   .provides('x', (leaf) => {
     const rnd = leaf.getRandGen('x')
-    return rnd.nextFloat(0.2, 1);
+    return rnd.nextFloat(0.5, 1);
   })
   .provides('y', (leaf) => {
     const rnd = leaf.getRandGen('y')
-    return rnd.nextFloat(0, .7);
+    return rnd.nextFloat(0, .6);
   })
   // angle from tip to side on right
   .provides('sa', (leaf) => {
     const rnd = leaf.getRandGen('sa')
-    return rnd.nextFloat(0.0, Math.PI/2);
+    return rnd.nextFloat(0.0, Math.PI * 2);
   })
   // angle from origin to side on right
   .provides('sb', (leaf) => {
     const rnd = leaf.getRandGen('sc')
-    return rnd.nextFloat(0.0, Math.PI/2);
+    return rnd.nextFloat(0.0, Math.PI * 2);
   })
   // angle from tip to side on left
   .provides('sc', (leaf) => {
@@ -88,6 +111,26 @@ var unilobe_without_basal = procedural('unilobe_without_basal')
       leaf.p1, leaf.p4, leaf.p3
     ]
   })
+  .provides('points_r', (leaf) => {
+    return [
+      leaf.p1,
+      leaf.t1,
+      leaf.p2,
+      leaf.t2,
+      leaf.p3,
+      leaf.t3
+    ]
+  })
+  .provides('points_l', (leaf) => {
+    return [
+      leaf.p1,
+      leaf.t6,
+      leaf.p4,
+      leaf.t5,
+      leaf.p3,
+      leaf.t3
+    ]
+  })
   // the total chord length of the right lobe
   .provides('D_r', (leaf) => {
     return chordLength(leaf.ps_r)
@@ -100,7 +143,7 @@ var unilobe_without_basal = procedural('unilobe_without_basal')
   .provides('us_r', (leaf) => {
     let us = [0];
     for (var i = 1; i < 3; i++) {
-      const d = distance(leaf.ps_r[i], leaf.ps_r[i-1]);
+      const d = distance(leaf.points_r[i], leaf.points_r[i-1]);
       const ui = us[i-1] + (d / leaf.D_r);
       us.push(ui);
     }
@@ -109,125 +152,58 @@ var unilobe_without_basal = procedural('unilobe_without_basal')
   .provides('us_l', (leaf) => {
     let us = [0];
     for (var i = 1; i < 3; i++) {
-      const d = distance(leaf.ps_l[i], leaf.ps_l[i-1]);
+      const d = distance(leaf.points_l[i], leaf.points_l[i-1]);
       const ui = us[i-1] + (d / leaf.D_l);
       us.push(ui);
     }
     return us;
   })
-  .provides('controlPoints_r', (l) => {
-    const us = l.us_r;
-    const D = l.D_r;
-
-    return [
-      l.p1, //q0
-      [
-        l.p1[0] + (D * l.t1[0] * (us[1] / 4)), // q1.x
-        l.p1[1] + (D * l.t1[1] * (us[1] / 4))  // q1.y
-      ],
-      [
-        l.p2[0] - (D * l.t2[0] * (us[1] / 4)), // q2.x
-        l.p2[1] - (D * l.t2[1] * (us[1] / 4))  // q2.y
-      ],
-      [
-        l.p2[0] + (D * l.t2[0] * ((1 - us[1]) / 4)), // q3.x
-        l.p2[1] + (D * l.t2[1] * ((1 - us[1]) / 4))  // q3.y
-      ],
-      [
-        l.p3[0] - (D * l.t3[0] * ((1 - us[1]) / 4)), // q4.x
-        l.p3[1] - (D * l.t3[1] * ((1 - us[1]) / 4))  // q4.y
-      ],
-      l.p3 // q5
+  .provides('knots_r', (leaf) => {
+    let vs = [0, 0, 0]
+    let n = leaf.ps_r.length;
+    for (var j = 1; j < n - 2 ; j++) {
+      vs[j+2] = 1;
+    }
+    vs = vs.concat([2, 2, 2]);
+    return vs;
+  })
+  .provides('side_l', (leaf) => {
+    const points = leaf.ps_l;
+    const knots = [
+      0, 0, 0, 2, 2, 2
     ];
-  })
-  .provides('controlPoints_l', (l) => {
-    const us = l.us_l;
-    const D = l.D_l;
-    return [
-      l.p1, //q0
-      [
-        l.p1[0] + (D * l.t6[0] * (us[1] / 4)), // q1.x
-        l.p1[1] + (D * l.t6[1] * (us[1] / 4))  // q1.y
-      ],
-      [
-        l.p4[0] - (D * l.t5[0] * (us[1] / 4)), // q2.x
-        l.p4[1] - (D * l.t5[1] * (us[1] / 4))  // q2.y
-      ],
-      [
-        l.p4[0] + (D * l.t5[0] * ((1 - us[1]) / 4)), // q3.x
-        l.p4[1] + (D * l.t5[1] * ((1 - us[1]) / 4))  // q3.y
-      ],
-      [
-        l.p3[0] - (D * l.t4[0] * ((1 - us[1]) / 4)), // q4.x
-        l.p3[1] - (D * l.t4[1] * ((1 - us[1]) / 4))  // q4.y
-      ],
-      l.p3
-    ]; //.reverse();
-  })
-  .provides('svgpath', (leaf) => {
-    const hw = leaf.width / 2;
-    const hh = leaf.height / 2;
-    const pts_l = leaf.controlPoints_l.map((p) => {
-      return [
-        hw + hw * p[0],
-        leaf.height - leaf.height * p[1]
-      ];
-    });
-    const pts_r = leaf.controlPoints_r.map((p) => {
-      return [
-        hw + hw * p[0],
-        leaf.height - leaf.height * p[1]
-      ];
-    });
-
-    const p2 = [
-      hw + leaf.p2[0] * hw,
-      leaf.height - leaf.p2[1] * leaf.height
-    ]
-
-    const p4 = [
-      hw + leaf.p4[0] * hw,
-      leaf.height - leaf.p4[1] * leaf.height
-    ]
-
-    var path = `M ${pts_r[0]}
-    Q ${pts_r[2]} ${p2}
-    T ${pts_r[5]} Z
-    M ${pts_l[0]}
-    Q ${pts_l[2]} ${p4}
-    T ${pts_l[5]} Z
-    `;
-    return path;
-  })
-  .provides('pointspath', (leaf) => {
-    const hw = leaf.width / 2;
+    const sampled_points = [];
+    const hw = leaf.width; // / 2;
     const hh = leaf.height;
+    for (var i = 0, l = leaf.quality; i < 1; i += (1/l)) {
+      const sp = bspline(i, 3, points, knots);
+      sampled_points.push([
+        hw/2 + sp[0] * hw,
+        hh - sp[1] * hh
+      ]);
+    }
 
-    const p1 = [
-      hw + leaf.p1[0] * hw,
-      hh - leaf.p1[1] * hh
-    ]
-
-    const p2 = [
-      hw + leaf.p2[0] * hw,
-      hh - leaf.p2[1] * hh
-    ]
-
-    const p3 = [
-      hw + leaf.p3[0] * hw,
-      hh - leaf.p3[1] * hh
-    ]
-
-    const p4 = [
-      hw + leaf.p4[0] * hw,
-      hh - leaf.p4[1] * hh
-    ]
-
-    path = `M ${p1} L ${p2} L ${p3} L ${p4} Z`;
-
-
-    return path;
+    return sampled_points;
   })
+  .provides('side_r', (leaf) => {
+    const points = leaf.ps_r;
+    const knots = [
+      0, 0, 0, 2, 2, 2
+    ];
+    const sampled_points = [];
+    const hw = leaf.width;// / 2;
+    const hh = leaf.height;
+    for (var i = 0, l = leaf.quality; i < 1; i += (1/l)) {
+      const sp = bspline(i, 3, points, knots);
+      sampled_points.push([
+        hw/2 + sp[0] * hw,
+        hh - sp[1] * hh
+      ]);
+    }
+
+
+    return sampled_points;
+  });
 
   function distance(p1, p2) {
     const dx = p1[0] - p2[0];
